fa5df75f05af2f03c73d0924554054b21e47c284
Magnus Kroner <magnus.kroner@gmail.com>
2024-03-06 18:22:17 +0100

    added fly cam

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
+mod pause_menu;
+mod fly_cam;
 
 fn main() {
     App::new()
         .init_state::<GameState>()
         .add_plugins(DefaultPlugins)
         .add_plugins(pause_menu::menu_plugin)
-        .add_systems(Startup, setup)
+        .add_plugins(fly_cam::FlyCamPlugin)
+        .add_systems(Startup, spawn_grass_tiles)
         .add_systems(Update, toggle_game_state)
         .run();
 }
@@ -15,10 +18,6 @@ enum GameState {
     PauseMenu,
     #[default]
     Game,
-}
-
-fn setup(mut cmd: Commands) {
-    cmd.spawn(Camera2dBundle::default());
 }
 
 fn toggle_game_state(
@@ -41,102 +40,17 @@ fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands
     }
 }
 
-mod pause_menu {
-    use super::{despawn_screen, GameState};
-    use bevy::{app::AppExit, prelude::*};
-
-    pub fn menu_plugin(app: &mut App) {
-        app.add_systems(OnEnter(GameState::PauseMenu), setup)
-            .add_systems(Update, button_system)
-            .add_systems(
-                OnExit(GameState::PauseMenu),
-                despawn_screen::<OnPauseMenuScreen>,
-            );
-    }
-
-    #[derive(Component)]
-    struct OnPauseMenuScreen;
-
-    fn setup(mut cmd: Commands, asset_server: Res<AssetServer>) {
-        cmd.spawn((
-            NodeBundle {
-                style: Style {
-                    flex_direction: FlexDirection::Column,
-                    width: Val::Percent(100.0),
-                    height: Val::Percent(100.0),
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                ..default()
-            },
-            OnPauseMenuScreen,
-        ))
-        .with_children(|parent| {
-            // Title
-            parent.spawn(
-                TextBundle::from_section(
-                    "House Build Game",
-                    TextStyle {
-                        font: asset_server.load("fonts/RobotoSlab.ttf"),
-                        font_size: 64.,
-                        color: Color::WHITE,
-                    },
-                )
-                .with_text_justify(JustifyText::Center)
-                .with_background_color(Color::RED.into())
-                .with_style(Style {
-                    top: Val::Percent(20.),
-                    ..Default::default()
-                }),
-            );
-            parent
-                .spawn(ButtonBundle {
-                    style: Style {
-                        top: Val::Percent(30.),
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    border_color: BorderColor(Color::BLACK),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(TextBundle::from_section(
-                        "Quit",
-                        TextStyle {
-                            font: asset_server.load("fonts/RobotoSlab.ttf"),
-                            font_size: 48.,
-                            color: Color::BLACK,
-                        },
-                    ));
-                });
-        });
-    }
-
-    fn button_system(
-        mut interaction_query: Query<
-            (&Interaction, &mut Style),
-            (Changed<Interaction>, With<Button>),
-        >,
-        mut exit: EventWriter<AppExit>,
-    ) {
-        for (interaction, mut style) in &mut interaction_query {
-            match *interaction {
-                Interaction::Pressed => {
-                    exit.send(AppExit);
-                }
-                Interaction::Hovered => {
-                    style.width = Val::Px(170.);
-                }
-                Interaction::None => {
-                    style.width = Val::Px(150.);
-                }
-            }
-        }
-    }
+fn spawn_grass_tiles(
+    mut cmd: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    cmd.spawn(
+        // ground plane
+        PbrBundle {
+            mesh: meshes.add(Plane3d::default().mesh().size(50.0, 50.0)),
+            material: materials.add(Color::SILVER),
+            ..default()
+        },
+    );
 }
